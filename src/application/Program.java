23e5908f11798c1d9f944b26365df6415164f05d
Joao Moraes <victorjbmoraes@gmail.com>
2026-05-13 16:55:38 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -34,6 +35,10 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-	}
+		System.out.println("\n=== Test 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = " + newSeller.getId());
+		}
 }
 
